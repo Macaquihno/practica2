bfd5557efc30140232d618fbc1f903f5df18a822
Macaquihno <fernangonza453@gmail.com>
2026-09-08 13:15:26 -0300

    ordenar codigo

--- practica profecional 2/mian.py
+++ practica profecional 2/mian.py
@@ -1,5 +1,5 @@
 import cargar_codigo
-
+import ordenar
 
 def main():
 
---
+++ practica profecional 2/ordenar.py
@@ -0,0 +1,27 @@
+def mostrar_codigos_ordenados(codigos):
+    """
+    Ordena y muestra los códigos utilizando el método de Burbuja.
+
+    Parámetros:
+        codigos (list): Lista de códigos de trazabilidad.
+
+    Retorno:
+        None
+    """
+
+    lista_ordenada = codigos[:]
+
+    # Método de Burbuja
+    for i in range(len(lista_ordenada) - 1):
+        for j in range(len(lista_ordenada) - 1 - i):
+
+            if lista_ordenada[j] > lista_ordenada[j + 1]:
+                auxiliar = lista_ordenada[j]
+                lista_ordenada[j] = lista_ordenada[j + 1]
+                lista_ordenada[j + 1] = auxiliar
+
+    print("\n--- CÓDIGOS ORDENADOS ---")
+
+    for codigo in lista_ordenada:
+        print(codigo)
+        
