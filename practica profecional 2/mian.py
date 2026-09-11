e7c5213a204dee8c94e228dd09d2fda2025b42f2
Macaquihno <fernangonza453@gmail.com>
2026-09-11 19:20:40 -0300

    Ordenar codigo funciona
    tuve que cambiar algunas varias cosas del main, en el ds puse el antes y el despues en el canal de sapojuegos, para cuando hacemos un jueguito

--- practica profecional 2/mian.py
+++ practica profecional 2/mian.py
@@ -3,21 +3,19 @@ import ordenar
 
 def main():
     codigos = []
-    print("Que accion desea realizar?\n1. Cargar codigo\n2. Mostrar codigos ordenados\n0. Salir")
-    valor_usuario = int(input(""))
+    valor_usuario = 9
 
     while valor_usuario != 0:
+        print("Que accion desea realizar?\n1. Cargar codigo\n2. Mostrar codigos ordenados\n0. Salir")
+        valor_usuario = int(input(""))
         if valor_usuario == 1:
             cargar_codigo.codigos(codigos)
             print(codigos)
-            
-            print("Que accion desea realizar?\n1. Cargar codigo\n2. Mostrar codigos ordenados\n0. Salir")
-            valor_usuario = int(input(""))
-            
-                
-
+            valor_usuario = 9       
+                            
         elif valor_usuario == 2:
             ordenar.mostrar_codigos_ordenados(codigos)
+            valor_usuario = 9
 
         
 
