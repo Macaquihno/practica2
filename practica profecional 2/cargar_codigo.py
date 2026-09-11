dd0e96bd28ae44f3a81f21f4df564989fab202e5
Alejo Max Barugel <alejobaruja@gmail.com>
2026-09-10 22:43:09 -0300

    Función que carga los códigos de trazabilidad cargada
    Pregunta al usuario la cantidad de códigos que desea ingresar.
            Pregunta al usuario que código desea ingresar en x posición.
            Agrega el código ingresado a la lista de códigos.
    
            Si el código ingresado es inválido, se le vuelve a preguntar al usuario que código desea ingresar en x posición.
     El sistema no pasará a la siguiente iteración si el código ingresado es inválido.

--- practica profecional 2/cargar_codigo.py
+++ practica profecional 2/cargar_codigo.py
@@ -1,9 +1,28 @@
 
-def codigos():
-    cadena_ingreso = str(input("Ingrese el código: "))
-    longitud = len(cadena_ingreso)
-    if cadena_ingreso == "" or longitud<4:
-        print("El código ingresado es inválido.")
-    else:
-        print("El código ingresado es válido.")
-        return cadena_ingreso
+def codigos(codigos):
+
+    """
+        Función que carga los códigos de trazabilidad.
+
+        Pregunta al usuario la cantidad de códigos que desea ingresar.
+        Pregunta al usuario que código desea ingresar en x posición.
+        Agrega el código ingresado a la lista de códigos.
+
+        Si el código ingresado es inválido, se le vuelve a preguntar al usuario que código desea ingresar en x posición.
+        El sistema no pasará a la siguiente iteración si el código ingresado es inválido.
+        """
+
+    cantidad_codigos = int(input("Ingrese la cantidad de códigos que desea ingresar: "))
+
+    for i in range(cantidad_codigos):
+        valido = False
+        while not valido:
+            cadena_ingreso = str(input(f"Ingrese el código {i+1}: "))
+            longitud = len(cadena_ingreso)
+
+            if cadena_ingreso == "" or longitud < 4:
+                print("El código ingresado es inválido.")
+            else:
+                print("El código ingresado es válido.")
+                codigos.append(cadena_ingreso)
+                valido = True
--- practica profecional 2/mian.py
+++ practica profecional 2/mian.py
@@ -2,20 +2,24 @@ import cargar_codigo
 import ordenar
 
 def main():
-
     codigos = []
-
-    valor_usuario = int(input("Ingrese el código: "))
+    print("Que accion desea realizar?\n1. Cargar codigo\n2. Mostrar codigos ordenados\n0. Salir")
+    valor_usuario = int(input(""))
 
     while valor_usuario != 0:
-
         if valor_usuario == 1:
             cargar_codigo.codigos(codigos)
+            print(codigos)
+            
+            print("Que accion desea realizar?\n1. Cargar codigo\n2. Mostrar codigos ordenados\n0. Salir")
+            valor_usuario = int(input(""))
+            
+                
 
         elif valor_usuario == 2:
             ordenar.mostrar_codigos_ordenados(codigos)
 
-        valor_usuario = int(input("Ingrese el código: "))
+        
 
     print("Adios")
 
